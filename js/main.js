8420b9c0f5cb929b2d7b665eee3a62508aba49c6
OUSSAMA <oussamacharef71@gmail.com>
2026-02-08 01:37:10 +0100

    add modification to files index.html and style.css and main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ let categoty_nav_list = document.querySelector(".category_nav_list");
 function Open_Categ_list(){
     categoty_nav_list.classList.toggle("active");
 }
+
+    var swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
