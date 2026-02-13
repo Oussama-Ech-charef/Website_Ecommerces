d5bf76b8d77384e34f2be5a4068329e900267aff
OUSSAMA <oussamacharef71@gmail.com>
2026-02-13 06:46:42 +0100

    add modification to files index.html and items_home.js

--- js/items_home.js
+++ js/items_home.js
@@ -4,7 +4,9 @@ fetch('products.json')
     console.log(data);
     
     const swiper_items_sale = document.getElementById("swiper_items_sale")
+    const swiper_elctronics = document.getElementById("swiper_elctronics")
 
+console.log(swiper_elctronics);
 
     data.forEach(product => {
         if(product.old_price) {
@@ -44,6 +46,62 @@ fetch('products.json')
                                 <span class="icon_product"><i class="fa-regular fa-heart"></i></span>
                              </div>
                     </div>`
+        }
+    })
+
+    data.forEach(product => {
+        if(product.catetory == "elctronics"){
+
+
+            const isInCart = cart.some(cartItem => cartItem.id === product.id)
+
+
+            const old_price_Pargrahp = product.old_price ? `<p class="old_price">$${product.old_price}</p>` : "";
+
+            const percent_disc_div = product.old_price ? `<span class="sale_present">%${Math.floor((product.old_price - product.price) / product.old_price * 100)}</span>` : "";
+
+
+
+            swiper_elctronics.innerHTML += `
+
+
+            <div class="swiper-slide product">
+                       
+                        ${percent_disc_div}
+                       <div class="img_product">
+                           <a href="#"><img src="${product.img}" alt=""></a>
+                       </div>
+
+                       <div class="stars">
+                           <i class="fa-solid fa-star"></i>
+                           <i class="fa-solid fa-star"></i>
+                           <i class="fa-solid fa-star"></i>
+                           <i class="fa-solid fa-star"></i>
+                           <i class="fa-solid fa-star"></i>
+                       </div>
+
+                       <p class="name_product"><a href="#">${product.name}</a></p>
+
+                       <div class="price">
+                           <p><span>$${product.price}</span></p>
+                           ${old_price_Pargrahp}
+                       </div>
+
+                       <div class="icons">
+                           <span class="btn_add_cart ${isInCart ? 'active' : ''}" data-id="${product.id}">
+                                <i class="fa-solid fa-cart-shopping"></i> ${isInCart ? 'Item in cart' : 'add to cart'}
+                            </span>
+                           <span class="icon_product"><i class="fa-regular fa-heart"></i></span>
+                       </div>
+                   </div>
+           
+           
+           
+           
+           `
+
+
+
         }
     })
 
