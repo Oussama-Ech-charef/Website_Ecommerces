9b2752e941ff270deb717a8987d5fd01ed9e2184
OUSSAMA <oussamacharef71@gmail.com>
2026-02-17 17:44:35 +0100

    feat: add modification to file main.js

--- js/main.js
+++ js/main.js
@@ -43,10 +43,42 @@ function addTocart(product) {
     console.log(product);
 
 
-    let cart = [];
+    let cart = JSON.parse(localStorage.getItem('cart')) || []
 
     cart.push({... product , quantity: 1})
     localStorage.setItem('cart', JSON.stringify(cart))
 
-    
+    updateCart()
+}
+
+
+
+function updateCart() {
+
+    let cartItemsContainer = document.getElementById("cart_items")
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || []
+
+
+    cartItemsContainer.innerHTML = "";
+    cart.forEach((item , index) => {
+        cartItemsContainer.innerHTML += `
+        
+        <div class="item_cart">
+                <img src="${item.img}" alt="online">
+                <div class="content">
+                    <h4>${item.name}</h4>
+                    <p class="price_cart">${item.price}</p>
+                    <div class="quantity_control">
+                        <button class="dectese_quantity">-</button>
+                        <span class="quantity">${item.quantity}</span>
+                        <button class="Increase_quantity">+</button>
+                    </div>
+                </div>
+
+                <button class="delete_item"><i class="fa-solid fa-trash-can"></i></button>
+            </div>
+        
+        `
+    })
 }
