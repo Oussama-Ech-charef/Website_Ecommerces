dabe8806c244a7239cfccd210de0d1b8bed9ea48
OUSSAMA <oussamacharef71@gmail.com>
2026-02-21 23:53:52 +0000

    add modification to file swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,29 +1,48 @@
-    var swiper = new Swiper(".slide-swp", {
-      pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-        clickable: true
-      },
-      autoplay:{
+var swiper = new Swiper(".slide-swp", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests: true,
+      clickable:true
+    },
+    autoplay:{
         delay:2500,
-      },
-      loop: true
-    });
+    },
+    loop:true
+  });
 
 
-    /* swiper slide products */
+  /* swiper slide products */
 
   var swiper = new Swiper(".slide_product", {
     slidesPerView: 5,
     spaceBetween:20,
     autoplay:{
         delay:2500,
-        
     },
     navigation:{
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
     },
     loop:true,
     watchOverflow: false,
-    });
+
+    breakpoints:{
+      1200:{
+        slidesPerView : 5,
+        spaceBetween: 20
+      },
+      1000:{
+        slidesPerView : 4,
+        spaceBetween: 20
+      },
+      700:{
+        slidesPerView: 3 , 
+        spaceBetween: 15 ,
+
+      },
+      0:{
+        slidesPerView : 2,
+        spaceBetween: 10
+      }
+    }
+  });
